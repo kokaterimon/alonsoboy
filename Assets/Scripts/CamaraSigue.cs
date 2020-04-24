d00808c4c6d5325c1542d7fd61f7a70d9de47ad2
kokaterimon <bpaucarm@outlook.com>
2020-04-23 21:04:09 -0500

    Terminando el tutorial juego

---
+++ Assets/Scripts/CamaraSigue.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamaraSigue : MonoBehaviour{
+
+    public Transform Objetivo;
+    public float suavizado = 5f;
+
+    Vector3 desface;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        desface = transform.position - Objetivo.position;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 posicionObjetivo = Objetivo.position + desface;
+        transform.position = Vector3.Lerp(transform.position, posicionObjetivo, suavizado * Time.deltaTime);
+    }
+}
